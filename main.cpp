e610b38da4279c61765d64a292fb12ab54831d1c
Helldir <argus0073@gmail.com>
2021-02-09 13:39:04 +0300

    0.0.4

--- main.cpp
+++ main.cpp
@@ -1,29 +1,45 @@
 #include <iostream>
 #include <iomanip>
-#include "header.h"
 
 using namespace std;
 
 int main(){
     //Первое задание
-    int a = 5;
-    int b = 9;
-    int c = 4;
-    int d = 3;
-    float f = static_cast<float>(a) * (static_cast<float>(b) + static_cast<float>(c) / static_cast<float>(d));
-    cout << "Result: " << setprecision(5) << f << endl;
+    int a;
+    cout << "Please enter a number: " << endl;
+    cin >> a;
+    if(a >= 10 && a <= 20)
+        cout << "10 <= " << a << " <= 20" << endl;
+    else
+        cout << a << " <= 10, or " << a << " >= 20" << endl;
 
     //Второе задание
     cout << "Enter a number: ";
     cin >> a;
-    (a > 21) ?  cout << "2 * (a - 21) = " << 2 * (a - 21) << endl : cout << "a - 21 = " << (a - 21) << endl;
+    int i;
+    for (i = (a - 1); i >= 1; i--){
+        if(!(a % i)){
+            break;
+        }
+    }
+    i == 1 ? cout << "Number is a simple." << endl :
+             cout << "Number is a not simple." << i << endl;
+
 
     //Третье задание
-    f = static_cast<float>(a_1) * (static_cast<float>(b_1) + static_cast<float>(c_1) / static_cast<float>(d_1));
-    cout << "Result number 2: " << setprecision(5) << f << endl;
+    cout << "Please enter a two numbers: " << endl;
+    cin >> a >> i;
+    if(a == 10 || i == 10 || (a + i) == 10)
+        cout << "True" << endl;
+    else
+        cout << "False" << endl;
 
     //Четвертое задание
-    int array[3][3][3] = {{{1,2,3},{1,2,3},{1,2,3}},{{1,2,3},{1,2,3},{1,2,3}},{{1,2,3},{1,2,3},{1,2,3}}};
-    cout << "PTR: " << *(*(*(array+1)+1)+1) << endl;
+    cout << "Enter a year: " << endl;
+    cin >> a;
+    if (!(a % 4) && ((a % 100) || !(a % 400)))
+        cout << "Leap year" << endl;
+    else
+        cout << "Not leap year" << endl;
     return 0;
 }
